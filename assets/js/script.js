58ae855d6a8537d684f9a26d9728f99fe1ab7606
Mian Baker <mian911@Mians-MacBook-Pro.local>
2023-11-20 20:41:22 -0500

    added hour block generating JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,36 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+function createHourBlock(time) {
+  var currentTime = 13
+  var relativeTime;
+  if (time > currentTime) {
+    relativeTime = "future"
+  } else if (time == currentTime) {
+    relativeTime = "present"
+  } else {
+    relativeTime = "past"
+  }
+  var hourBlock = $('<div class="row time-block ' + relativeTime + '">')
+  var timeDisplay = $('<div class="col-2 col-md-1 hour text-center py-3">').text(time)
+  var textInput = $('<textarea class="col-8 col-md-10 description" rows="3">')
+  var saveButton = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save">').append($('<i class="fas fa-save" aria-hidden="true"></i>'))
+
+  hourBlock.append(timeDisplay, textInput, saveButton)
+  return hourBlock;
+}
+
+$(document).ready(function () {
+  console.log("jQuery loaded.")
+  var mainBox = $("div.container-fluid.px-5")
+  mainBox.html(null)
+
+  var times = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+  for (var time of times) {
+    var hourBlock = createHourBlock(time)
+    mainBox.append(hourBlock)
+  }
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +49,13 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+
+  //    <div id="hour-9" class="row time-block past">
+  //      <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
+  //      <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+  //      <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+  //        <i class="fas fa-save" aria-hidden="true"></i>
+  //      </button>
+  //    </div>
 });
