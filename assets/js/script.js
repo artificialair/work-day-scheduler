463f0964d47a98f5c8784ac958309dfb05bc37fc
Mian Baker <mian911@mians-mbp.lan>
2023-11-22 21:21:53 -0500

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// constants for the hours the scheduler will span (mostly used for testing color coding)
 const startHour = 9
 const endHour = 17
 
@@ -13,19 +11,23 @@ if (tasks === null) {
 }
 
 function loadLocalStorage() {
+  // I tried saving a list to localStorage and it saved weirdly, iterating over an object is just easier on my end
   for (var [taskLocation, message] of Object.entries(tasks)) {
+    // finds the text area in the hour block with `taskLocation` as it's id and sets it's value to `message`
     hourBlock = $(taskLocation)
     hourBlock.find(".description").val(message)
   }
 }
 
 function displayHeader() {
+  // Gets and formats date and time from dayJS and displays it in the header
   header = $("#currentDay")
   var now = dayjs().format("dddd, MMMM DD")
   header.text(now)
 }
 
 function createHourBlock(time) {
+  // Compares the time of the block to the current time, and sets the class as past/present/future
   var relativeTime;
   if (time > currentTime) {
     relativeTime = "future"
@@ -35,6 +37,8 @@ function createHourBlock(time) {
     relativeTime = "past"
   }
 
+  // since time is stored in 24h, this converts it to 12h and adds an AM or PM 
+  // this will break if startHour = 0, not fixing as it will not be relevant in the final submission
   var displayTime;
   if (time > 12) {
     displayTime = time - 12
@@ -47,6 +51,7 @@ function createHourBlock(time) {
     displayTime += "AM"
   }
 
+  // creates and returns a new hour block with the above data
   var hourBlock = $('<div id="hour-'+time+'" class="row time-block ' + relativeTime + '">')
   var timeDisplay = $('<div class="col-2 col-md-1 hour text-center py-3">').text(displayTime)
   var textInput = $('<textarea class="col-8 col-md-10 description" rows="3">')
@@ -56,11 +61,14 @@ function createHourBlock(time) {
   return hourBlock;
 }
 
+// this function runs when jQuery is fully loaded
 $(document).ready(function () {
   var mainBox = $("div.container-fluid")
+  // Clears the main box
+  // I'm pretty sure this is unnecessary
   mainBox.html(null)
 
-  // Generates the webpage, from 9 to 5 (startHour to endHour)
+  // Generates the main section of the webpage, from 9 to 5 (startHour to endHour)
   for (var i = startHour; i <= endHour; i++) {
     var hourBlock = createHourBlock(i)
     mainBox.append(hourBlock)
@@ -70,15 +78,19 @@ $(document).ready(function () {
   displayHeader()
 
 $(".saveBtn").on("click", function(event) {
+  // gets the div the clicked button is in
   buttonParent = $(this).parent()
 
+  // gets the id of the div
   taskLocation = "#" + buttonParent.attr("id")
+  // gets the message of the textarea within the div
   message = buttonParent.find("textarea.description").val()
 
+  // adds a field to the "tasks" object
+  // if the field already exists this will reset it
   tasks[taskLocation] = message
   localStorage.setItem("scheduleInputs", JSON.stringify(tasks))
-  
+
 })
 
-  // TODO: Add code to display the current date in the header of the page.
 });
