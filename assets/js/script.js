70a802328ac4349b3bc361fa1853766d1d6211fe
Mian Baker <mian911@mians-mbp.lan>
2023-11-22 20:51:10 -0500

    made the save button work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,25 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+const startHour = 9
+const endHour = 17
+
+var currentTime = dayjs().hour()
+var tasks = localStorage.getItem("scheduleInputs")
+if (tasks === null) {
+  tasks = {}
+} else {
+  tasks = JSON.parse(tasks)
+}
+
+function loadLocalStorage() {
+  for (var [taskLocation, message] of Object.entries(tasks)) {
+    hourBlock = $(taskLocation)
+    hourBlock.find(".description").val(message)
+  }
+}
+
 function createHourBlock(time) {
-  var currentTime = 13
   var relativeTime;
   if (time > currentTime) {
     relativeTime = "future"
@@ -11,25 +28,51 @@ function createHourBlock(time) {
   } else {
     relativeTime = "past"
   }
-  var hourBlock = $('<div class="row time-block ' + relativeTime + '">')
-  var timeDisplay = $('<div class="col-2 col-md-1 hour text-center py-3">').text(time)
+
+  var displayTime;
+  if (time > 12) {
+    displayTime = time - 12
+  } else {
+    displayTime = time
+  }
+  if (time > 11) {
+    displayTime += "PM"
+  } else {
+    displayTime += "AM"
+  }
+
+  var hourBlock = $('<div id="hour-'+time+'" class="row time-block ' + relativeTime + '">')
+  var timeDisplay = $('<div class="col-2 col-md-1 hour text-center py-3">').text(displayTime)
   var textInput = $('<textarea class="col-8 col-md-10 description" rows="3">')
-  var saveButton = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save">').append($('<i class="fas fa-save" aria-hidden="true"></i>'))
+  var saveButton = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save">').append($('<i class="fas fa-save" aria-hidden="true">'))
 
   hourBlock.append(timeDisplay, textInput, saveButton)
   return hourBlock;
 }
 
 $(document).ready(function () {
-  console.log("jQuery loaded.")
-  var mainBox = $("div.container-fluid.px-5")
+  var mainBox = $("div.container-fluid")
   mainBox.html(null)
 
-  var times = [9, 10, 11, 12, 13, 14, 15, 16, 17]
-  for (var time of times) {
-    var hourBlock = createHourBlock(time)
+  // Generates the webpage, from 9 to 5 (startHour to endHour)
+  for (var i = startHour; i <= endHour; i++) {
+    var hourBlock = createHourBlock(i)
     mainBox.append(hourBlock)
   }
+
+  loadLocalStorage()
+
+$(".saveBtn").on("click", function(event) {
+  buttonParent = $(this).parent()
+
+  taskLocation = "#" + buttonParent.attr("id")
+  message = buttonParent.find("textarea.description").val()
+
+  tasks[taskLocation] = message
+  localStorage.setItem("scheduleInputs", JSON.stringify(tasks))
+})
+
+
   
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
