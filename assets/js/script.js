32efe73f4851d68a3771ccbe9c486f1dc8bcbc4d
Mian Baker <mian911@mians-mbp.lan>
2023-11-22 21:05:53 -0500

    added date to header

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,12 @@ function loadLocalStorage() {
   }
 }
 
+function displayHeader() {
+  header = $("#currentDay")
+  var now = dayjs().format("dddd, MMMM DD")
+  header.text(now)
+}
+
 function createHourBlock(time) {
   var relativeTime;
   if (time > currentTime) {
@@ -61,6 +67,7 @@ $(document).ready(function () {
   }
 
   loadLocalStorage()
+  displayHeader()
 
 $(".saveBtn").on("click", function(event) {
   buttonParent = $(this).parent()
@@ -70,35 +77,8 @@ $(".saveBtn").on("click", function(event) {
 
   tasks[taskLocation] = message
   localStorage.setItem("scheduleInputs", JSON.stringify(tasks))
+  
 })
 
-
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
   // TODO: Add code to display the current date in the header of the page.
-
-
-  //    <div id="hour-9" class="row time-block past">
-  //      <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
-  //      <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-  //      <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-  //        <i class="fas fa-save" aria-hidden="true"></i>
-  //      </button>
-  //    </div>
 });
